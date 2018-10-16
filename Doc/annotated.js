0a3b4b200f90bd0a606c6424ea20409327c43952
Chris Hettrick <git@structfoo.com>
2018-10-16 16:42:02 -0700

    RTX Real-Time Operating System V4.82 from Keil as packaged in V5.0.0 of ARM.CMSIS.5.0.0.pack. All files ran through "dos2unix" to fix line endings.
    Released on 2016-11-11. Bug fix release.
    Corrected: Recursive Mutex 16-bit lock counter is now checked to not overflow.

--- Doc/annotated.js
+++ Doc/annotated.js
@@ -1,10 +1,12 @@
 var annotated =
 [
-    [ "osMailQDef_t", "structos_mail_q_def__t.html", "structos_mail_q_def__t" ],
-    [ "osMessageQDef_t", "structos_message_q_def__t.html", "structos_message_q_def__t" ],
-    [ "osMutexDef_t", "structos_mutex_def__t.html", "structos_mutex_def__t" ],
-    [ "osPoolDef_t", "structos_pool_def__t.html", "structos_pool_def__t" ],
-    [ "osSemaphoreDef_t", "structos_semaphore_def__t.html", "structos_semaphore_def__t" ],
-    [ "osThreadDef_t", "structos_thread_def__t.html", "structos_thread_def__t" ],
-    [ "osTimerDef_t", "structos_timer_def__t.html", "structos_timer_def__t" ]
+    [ "os_mailQ", "group__CMSIS__RTOS__Definitions.html#structos__mailQ", null ],
+    [ "osEvent", "group__CMSIS__RTOS__Definitions.html#structosEvent", "group__CMSIS__RTOS__Definitions_structosEvent" ],
+    [ "osMailQDef_t", "structosMailQDef__t.html", "structosMailQDef__t" ],
+    [ "osMessageQDef_t", "structosMessageQDef__t.html", "structosMessageQDef__t" ],
+    [ "osMutexDef_t", "structosMutexDef__t.html", "structosMutexDef__t" ],
+    [ "osPoolDef_t", "structosPoolDef__t.html", "structosPoolDef__t" ],
+    [ "osSemaphoreDef_t", "structosSemaphoreDef__t.html", "structosSemaphoreDef__t" ],
+    [ "osThreadDef_t", "structosThreadDef__t.html", "structosThreadDef__t" ],
+    [ "osTimerDef_t", "structosTimerDef__t.html", "structosTimerDef__t" ]
 ];
